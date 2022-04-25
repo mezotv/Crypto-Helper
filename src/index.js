f39d6dad47471e9b0d1a6a24bba9e9b1acbda050
Mezo <dominik@koch-bautechnik.de>
2022-04-25 18:20:54 +0200

    Removed intents that arent needed

--- src/index.js
+++ src/index.js
@@ -7,14 +7,11 @@ const discordModals = require('discord-modals')
 const client = new Client({
   intents: [
     "GUILDS",
-    "GUILD_MEMBERS",
     "GUILD_BANS",
     "GUILD_INTEGRATIONS",
     "GUILD_WEBHOOKS",
     "GUILD_INVITES",
     "GUILD_VOICE_STATES",
-    "GUILD_PRESENCES",
-    "GUILD_MESSAGES",
     "GUILD_MESSAGE_REACTIONS",
     "GUILD_MESSAGE_TYPING",
     "DIRECT_MESSAGES",
