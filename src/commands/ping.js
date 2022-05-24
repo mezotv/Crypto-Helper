3c827bbd10269c7310d609a28365af9777380bfe
Mezo <dominik@koch-bautechnik.de>
2022-05-24 16:26:32 +0200

    Ping.js update

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,39 +1,49 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Returns the bot's ping status"),
+    .setDescription("Displays the clients ping"),
 
-  async execute(interaction, client) {
-    const pingembed = new MessageEmbed()
-      .setAuthor({
-        name: `${client.user.username}`,
-        iconURL: client.user.avatarURL(),
-      })
-      .setColor("#5865f4")
-      .setTitle(":ping_pong:  Pong!")
-      .addFields(
-        {
-          name: "**Shard 1** latency",
-          value: `> **${Math.abs(
-            Date.now() - interaction.createdTimestamp
-          )}**ms`,
-          inline: false,
-        },
-        {
-          name: "**Api** latency",
-          value: `> **${Math.round(client.ws.ping)}**ms`,
-          inline: false,
-        }
-      )
-
-      .setTimestamp()
-      .setFooter({ text: "Crypto Helper made by Mezo#0001" });
-
-    await interaction.reply({
-      embeds: [pingembed],
-    });
-  },
-};
+    async execute(interaction, client) {
+      const pingembed = new MessageEmbed()
+  
+        .setColor("#5865f4")
+        .setTitle(":ping_pong:  Pong!")
+        .addFields(
+          {
+            name: "**Client** latency",
+            value: `> **${Math.abs(
+              Date.now() - interaction.createdTimestamp
+            )}**ms`,
+            inline: false,
+          },
+          {
+            name: "**Api** latency",
+            value: `> **${Math.round(client.ws.ping)}**ms`,
+            inline: false,
+          }
+        )
+        const button = new MessageActionRow().addComponents(
+          new MessageButton()
+            .setLabel("Discord latency")
+            .setStyle("LINK")
+            .setEmoji("💻")
+            .setURL(
+              'https://discordstatus.com/'
+            ),
+        );
+        
+      await interaction.reply({
+        embeds: [pingembed],
+        components: [button]
+      });
+      setTimeout(function () {
+        button.components[0].setDisabled(true);
+        interaction.editReply({embeds: [pingembed], components: [button]})
+  
+      }, 120000);
+    },
+  };
+  
