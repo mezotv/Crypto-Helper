5127722abb8fce9f941ad79b42df6a8aa80cbaf8
Mezo <dominik@koch-bautechnik.de>
2022-07-15 12:41:55 +0200

    Coin.js final update

--- src/commands/coin.js
+++ src/commands/coin.js
@@ -34,12 +34,26 @@ module.exports = {
       coinembed = new MessageEmbed()
         .setColor('RED')
         .setTitle('Error')
-        .setDescription('Could not find coin with the given id');
+        .setDescription('Could not find a coin with the given id');
     } else {
       coinembed = new MessageEmbed()
         .setColor('RANDOM')
         .setTitle(`Coin: ${datacrypto.name}`)
-        .setDescription(`Price: ${datacrypto.market_data.current_price.usd}$`);
+        .setThumbnail(datacrypto.image.large)
+        .addFields(
+          { name: 'Symbol', value: datacrypto.symbol, inline: false },
+          { name: 'Price in USD', value: `$${datacrypto.market_data.current_price.usd}`, inline: true },
+          { name: 'Price in EUR', value: `${datacrypto.market_data.current_price.eur}€`, inline: true },
+          { name: '24h Price Change', value: `${datacrypto.market_data.price_change_percentage_24h}%`, inline: true },
+          { name: 'Volume', value: `$${datacrypto.market_data.total_volume.usd}`, inline: true },
+          { name: 'Circulating Supply', value: `${datacrypto.market_data.circulating_supply}`, inline: true },
+          { name: 'Total Supply', value: `${datacrypto.market_data.total_supply}`, inline: true },
+          { name: 'Website', value: `[${datacrypto.links.homepage[0]}](${datacrypto.links.homepage[0]})`, inline: true },
+        )
+        .setTimestamp()
+        .setFooter({
+          text: 'Crypto Helper made by Developer Dungeon Studios',
+        });
     }
     await interaction.reply({
       embeds: [coinembed],
